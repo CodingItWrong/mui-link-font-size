3e1d5a5e89068040ee25eb4916021c5c10e74d96
Josh Justice <me@codingitwrong.com>
2022-09-27 10:18:25 -0400

    Apply theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+
+const fontScaleFactor = 1.6; // to undo the base CSS 62.5% font size on the root element, html
+const theme = createTheme({
+  typography: {
+    fontSize: 14 * fontScaleFactor,
+  },
+});
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <div>
         <Button variant="contained">Button</Button>
       </div>
@@ -14,7 +22,7 @@ function App() {
       <div>
         <Typography>Typography</Typography>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
